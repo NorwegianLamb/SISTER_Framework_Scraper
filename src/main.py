8b9e0baec881206ba7c61aa8d71c8b3c97c71fbc
NorwegianLamb <norwegianlamb@protonmail.com>
2023-08-23 14:30:26 +0200

    choosed headless browser to automate the script
    unfortunately after the login process with the previous version, I had problems with figuring out how to bypass the FW checks, unfortunately requests does not load JS script when GET requesting

--- src/main.py
+++ src/main.py
@@ -1,117 +1,57 @@
-''' 
-Author: Flavio Gjoni
-Description: Automated scraper for the SISTER framework, it gathers the PDF files for property successions and parse them
-Version: 1.0x
-'''
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
 from bs4 import BeautifulSoup
 import argparse
 import time
 
-# -------------------------------------------------------------------------------------------------------------------------------
-
+print("hello")
+# Headless chrome browser + OPTIONS() settings + base_url
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 base_url = "https://portalebanchedaticdl.visura.it"
-session = requests.Session()
-session.debug = True # Comment in PROD
-
-# CHECKER_FUNCS -------------------------------------------------------------------------------------------------------------------------------
 
 
-def getResponseInfo(response, getContent=False):
-    print(f"Response headers: {response.headers}")
-    cookies_str = "\n".join([str(cookie) for cookie in session.cookies]) # this for sure has a better way to be done
-    print(f"Cookies:\n{cookies_str}")
-    if(getContent):
-        print(f"Content:\n{response.text}")
-
-
-
-def setBaseCookies():
-    response = session.get(base_url + "/homepageAreeTematicheAction.do")
-    if response.status_code == 200:
-        # getResponseInfo(response)
-        pass
-    else:
-        print("volevi, es ist schwer digga :'c")
-
-
-
-def temp_LogoutButtonPresent(response_text):
-    soup = BeautifulSoup(response_text, 'html.parser')
-    logout_button = soup.find('input', {'type': 'button', 'value': 'Logout', 'onclick': "location.href='/actionLogOutNuovoVisura.do?url=sito'"})
-    print(logout_button is not None)
-
-
-# LOGIN/LOGOUT -------------------------------------------------------------------------------------------------------------------------------
-
-
+# Login function (entry function in main())
 def login(username, password):
-    # setting basic SESSION token
-    setBaseCookies()
-    # defining POST request data
     base_url_login = base_url + "/authenticateNuovoVisura.do?url=sito"
-    login_data = {
-        'userName': username,
-        'password': password,
-        'firstPage': '/homepageBancheDatiActionNuovoVisura.do'
-    }
-    # sending LOGIN POST request
-    login_response = session.post(base_url_login, data=login_data)
-    if login_response.status_code == 200:
-        if "Credenziali errate" in login_response.text:
-            print("Invalid credentials message found.")
-            print("Request:")
-            print(login_response.request.headers)
-            print(login_response.request.body)
-        else:
-            # getResponseInfo(login_response, True)
-            temp_LogoutButtonPresent(login_response.text)
-            logout()
+    driver.get(base_url_login)
+    driver.maximize_window()
+
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located(("name", 'userName')))
+
+    # Find the login form elements and submit the login
+    username_field = driver.find_element("name", 'userName')
+    password_field = driver.find_element("name", "password")
+    login_button = driver.find_element("XPATH", '//*[@id="testata"]/div/div[2]/div[1]/button')
+    username_field.send_keys(str(username))
+    password_field.send_keys(str(password))
+    login_button.click()
+
+    # Check if "Credenziali errate" is present on the page
+    if "Credenziali errate" in driver.page_source:
+        print("Invalid credentials!")
     else:
-        print("volevi pt.2, es ist nicht einfach :'c")
-
-
+        pass
 
 def logout():
     print("\nWaiting 2 seconds for the logout to AVOID detection...")
-    time.sleep(2)
-    # defining GET request for logging out
-    logout_url = base_url + "/actionLogOutNuovoVisura.do?url=sito"
-    logout_response = session.get(logout_url)
-    if logout_response.status_code == 200:
-        # getResponseInfo(logout_response, True)
-        pass
-    else:
-        print("bro how can you be so bad T_T")
-    # closing requests.Session()
-    session.close()
-    print("Logout and Sessions closed, goodbye :)")
+    driver.implicitly_wait(2)
+    driver.get(base_url + "/actionLogOutNuovoVisura.do?url=sito")
+    # Close the WebDriver instance
+    driver.quit()
+    print("Logout and WebDriver closed, goodbye :)")
 
-
-# QUERY/PARSER -------------------------------------------------------------------------------------------------------------------------------
-
-
-def searchData(query):
-    data = {}
-    response = session.post(base_url, data=data)
-    if response.status_code == 200:
-       downloadFile()
-    else:
-        pass
-
-
-
-def downloadFile(url):
-    pass
-
-
-# ENTRY_POINT -------------------------------------------------------------------------------------------------------------------------------
+# Other functions remain unchanged
 
 if __name__ == "__main__":
-    #'''
-    parser = argparse.ArgumentParser(description="Automated scraper for the SISTER framework, it gathers the PDF files for property successions and parse them")
+    parser = argparse.ArgumentParser(description="Automated scraper for the SISTER framework, it gathers the PDF files for property successions and parses them")
     parser.add_argument("-u", "--username", required=True, help="Username for login")
     parser.add_argument("-p", "--password", required=True, help="Password for login")
     args = parser.parse_args()
-    #'''
     login(args.username, args.password)
