41eabb1ba0e0d2ca88519134db68d694e9e48f52
NorwegianLamb <norwegianlamb@protonmail.com>
2023-09-03 20:11:58 +0200

    added to-do for tomorrow

--- src/test.py
+++ src/test.py
@@ -9,6 +9,29 @@ download_directory = os.path.join(current_directory, 'data')
 renamed_directory = os.path.join(download_directory, 'renamed')
 file_path = os.path.join(renamed_directory, f'nota-{nota}.pdf')
 
-for page_layout in extract_pages(file_path):
-    for element in page_layout:
-        print(element)
+# from what I see here we will have to REGEX the 'unità immobiliare n.{regex express}' and loop them to save them
+# we should do the same with the 'intestati -> {num}. [text]'
+
+text = extract_text(file_path)
+
+#here comes my nightmare: regex
+pattern = re.compile(r"Unità immobiliare")
+matches = pattern.findall(text)
+"""
+WHAT DO WE NEED FROM THAT FILE?
+
+    -> UNITA' IMMOBILIARE N°<num>: <PARSE INFO> (CATASTO, FOGLIO, PARTICELLA, SUBALTERNO)
+        -> THESE ARE GOING TO BE INSERTED IN "Ricerca per Immobili" + "intestati" 
+            -> se IMMOBILE trovato è lo stesso dell'INTESTATO/I a fine PDF aggiungi, altrimenti inserisci "NO"
+                -> SE RISULTATO = "NO" uscire dal file PDF (anche se solo 1 no? è possibile?) -> continuare il loop delle note
+            -> SE IMMOBILE è lo stesso, ci salviamo "UBICAZIONE", "CLASSAMENTO", "CONSISTENZA" + "ALTRE UBICAZIONI, CONST.." + ...
+
+    -> INTESTATI:
+        -> n.1, n.2, ..., n.<num>
+            -> SALVA NOME, CF, DIRITTO DI PROPRIETA' -> da qui capire chi ha il maggior diritto e filtri finali
+"""
+
+
+print(matches)
+
+# currently not needed an IMAGE extractor, only text is sufficient, maybe parse the images in the PDF so we can delete useless info?
